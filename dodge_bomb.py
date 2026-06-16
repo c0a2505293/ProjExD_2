d5adb284843abb915c22645d80929566d9b899ce
ogawa <c0a2505293@edu.teu.ac.jp>
2026-06-16 16:43:46 +0900

    爆弾の速度大きさ

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -16,17 +16,14 @@ os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
 
 def check_bound(rct: pg.Rect) -> tuple[bool, bool]:
-    """
-    引数：こうかとんRect or 爆弾Rect
-    戻り値：判定結果タプル（横方向判定結果，縦方向判定結果）
-    True：画面内／False：画面外
-    """
     yoko, tate = True, True
-    if rct.left < 0 or WIDTH < rct.right:  # 横方向判定
+    if rct.left < 0 or WIDTH < rct.right:
         yoko = False
-    if rct.top < 0 or HEIGHT < rct.bottom:  # 縦方向判定
+    if rct.top < 0 or HEIGHT < rct.bottom:
         tate = False
     return yoko, tate
+
+
 
 def game_over(screen: pg.Surface) -> None:
     black = pg.Surface((WIDTH, HEIGHT))
@@ -55,9 +52,20 @@ def game_over(screen: pg.Surface) -> None:
     pg.display.update()
     time.sleep(5)
 
-
-
-
+def init_bb_imgs() -> tuple[list[pg.Surface], list[int]]:
+    """
+    10段階の大きさを変えた爆弾Surfaceのリストと加速度のリストを準備する
+    戻り値：(爆弾Surfaceのリスト, 加速度のリスト)
+    """
+    bb_imgs = []
+    for r in range(1, 11):
+        bb_img = pg.Surface((20 * r, 20 * r))
+        bb_img.set_colorkey((0, 0, 0)) 
+        pg.draw.circle(bb_img, (255, 0, 0), (10 * r, 10 * r), 10 * r)
+        bb_imgs.append(bb_img)
+    
+    bb_accs = [a for a in range(1, 11)]
+    return bb_imgs, bb_accs    
 
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
@@ -67,9 +75,10 @@ def main():
     kk_rct = kk_img.get_rect()
     kk_rct.center = 300, 200
 
-    bb_img = pg.Surface((20, 20))  # 爆弾用の空のSurface
-    pg.draw.circle(bb_img, (255, 0, 0), (10, 10), 10)  # 半径10の赤い円を描画
-    bb_img.set_colorkey((0, 0, 0))
+
+    bb_imgs, bb_accs = init_bb_imgs()    
+   
+    bb_img = bb_imgs[0]
     bb_rct = bb_img.get_rect()  # 爆弾Rect
     bb_rct.centerx = random.randint(0, WIDTH)  # 横初期座標
     bb_rct.centery = random.randint(0, HEIGHT)  # 縦初期座標
@@ -108,11 +117,20 @@ def main():
         screen.blit(kk_img, kk_rct)
         
         yoko, tate = check_bound(bb_rct)
-        if not yoko:  # 横方向にはみ出ていたら
+        if not yoko:
             vx *= -1
-        if not tate:  # 縦方向にはみ出ていたら
+        if not tate:
             vy *= -1
-        bb_rct.move_ip(vx, vy)
+
+        idx = min(tmr//500, 9)
+        bb_img = bb_imgs[idx]
+        avx = vx * bb_accs[idx]
+        avy = vy * bb_accs[idx]
+
+
+        bb_rct.width = bb_img.get_rect().width
+        bb_rct.height = bb_img.get_rect().height
+        bb_rct.move_ip(avx, avy)
         screen.blit(bb_img, bb_rct)
         pg.display.update()
         tmr += 1
