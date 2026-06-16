9d62fafde04eb3bdd99aad1b263bc94b403056de
ogawa <c0a2505293@edu.teu.ac.jp>
2026-06-16 14:09:22 +0900

    練習１

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -4,6 +4,12 @@ import pygame as pg
 
 
 WIDTH, HEIGHT = 1100, 650
+DELTA = {
+    pg.K_UP: (0, -5),
+    pg.K_DOWN: (0, +5),
+    pg.K_LEFT: (-5, 0),
+    pg.K_RIGHT: (+5, 0),
+    }
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
 
@@ -32,6 +38,10 @@ def main():
             sum_mv[0] -= 5
         if key_lst[pg.K_RIGHT]:
             sum_mv[0] += 5
+        for key, mv in DELTA.items():
+            if key_lst[key]:
+                sum_mv[0] += mv[0]
+                sum_mv[1] += mv[1]
         kk_rct.move_ip(sum_mv)
         screen.blit(kk_img, kk_rct)
         pg.display.update()
